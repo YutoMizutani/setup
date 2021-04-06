abea696b1c9b1e65813aa8ce97eb84980ecfb70f
YutoMizutani <yutomizutani@YutonoMacBook-Pro.local>
2021-04-06 22:15:21 +0900

    Fix some

--- scripts/01/terminal/zsh/install-zsh.sh
+++ scripts/01/terminal/zsh/install-zsh.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/zsh
 
 sh $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/../../../utils/log/info.sh $0
 
@@ -7,6 +7,7 @@ cp .bashrc .zshrc
 cp .bash_profile .zprofile
 
 brew install zsh zsh-autosuggestions zsh-completions zsh-syntax-highlighting autojump peco
+echo source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh >> ~/.zshrc
 echo source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh >> ~/.zshrc
 echo /usr/local/bin/zsh | sudo tee -a /etc/shells
 chsh -s /usr/local/bin/zsh
@@ -14,13 +15,15 @@ chsh -s /usr/local/bin/zsh
 # https://qiita.com/kinchiki/items/57e9391128d07819c321
 # Apply prezto
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+zsh -c 'setopt EXTENDED_GLOB'
+for rcfile in "${ZDOTDIR:-$HOME}/.zprezto/runcoms/^README.md(.N)"; do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
+# Install prompt
+npm install --global pure-prompt
 # Apply Theme pure
-sudo sed -i "" -e "s/'sorin'/'pure'/" -e "s/'completion' \\/'pure'/" ~/.zprezto/runcoms/zpreztorc
+sudo sed -i "" -e "s/'sorin'/'pure'/" -e "s/'completion' \\\/'pure'/" ~/.zprezto/runcoms/zpreztorc
 # Add some plugins
 sudo sed -i "" -e "s/'completion'/'completion' \\\\LF  'syntax-highlighting' \\\\LF  'autosuggestions'/" ~/.zprezto/runcoms/zpreztorc
 LF=$'\\\x0A'
--- scripts/03/zsh/restore-rc.sh
+++ scripts/03/zsh/restore-rc.sh
@@ -3,4 +3,4 @@
 sh $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/../../utils/log/info.sh $0
 
 mv ~/.zshrc ~/.zshrc.bak
-curl https://gist.githubusercontent.com/YutoMizutani/5864ffac249cfcd433c7feb26cf6b687/raw/f05dd237d695063bde9b3509c74274a5ff114327/.zshrc > .zshrc
+curl https://gist.githubusercontent.com/YutoMizutani/5864ffac249cfcd433c7feb26cf6b687/raw/f05dd237d695063bde9b3509c74274a5ff114327/.zshrc > ~/.zshrc
